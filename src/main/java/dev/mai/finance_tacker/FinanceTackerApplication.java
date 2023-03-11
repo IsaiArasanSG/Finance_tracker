41979df2bc6aae3182e3891a93150d7e49096e04
Myleshen Praburam <mylesuge@gmail.com>
2023-03-11 14:06:11 +0530

    Initial Commit with boilerplate

---
+++ src/main/java/dev/mai/finance_tacker/FinanceTackerApplication.java
@@ -0,0 +1,13 @@
+package dev.mai.finance_tacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTackerApplication.class, args);
+	}
+
+}
